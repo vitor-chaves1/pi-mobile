bc761bd510561f70b761ef376bfe311d298783cf
Vitor Chaves <vitor.chaves@iesb.edu.br>
2023-10-09 23:22:22 -0300

    #59 adicionando tela registrar

--- App.js
+++ App.js
@@ -7,11 +7,12 @@ import Historico from './screens/Historico';
 import ListaDesejos from './screens/ListaDesejos';
 import Perfil from './screens/Perfil';
 import Login from './screens/Login';
+import Registrar from './screens/Registrar';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Login/>
+      <Registrar/>
     </SafeAreaProvider>
   );
 }
