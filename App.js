3624cf3ba0320675938b9bcbdfc7ad4c6690c5d2
Vitor Chaves <vitor.chaves@iesb.edu.br>
2023-10-09 23:09:34 -0300

    #58 adicionando tela login

--- App.js
+++ App.js
@@ -6,13 +6,12 @@ import Carrinho from './screens/Carrinho';
 import Historico from './screens/Historico';
 import ListaDesejos from './screens/ListaDesejos';
 import Perfil from './screens/Perfil';
+import Login from './screens/Login';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Navbar />
-      
-      <Perfil />
+      <Login/>
     </SafeAreaProvider>
   );
 }
